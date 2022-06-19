78eb99d054249238223db93906825c156993b00f
Jiyur <53509652+Jiyur@users.noreply.github.com>
2022-06-19 21:52:34 +0700

    merge XSS branch

--- src/main/java/util/HibernateUtil2.java
+++ src/main/java/util/HibernateUtil2.java
@@ -18,9 +18,9 @@ public class HibernateUtil2{
 
                 Properties settings = new Properties();
                 settings.put(Environment.DRIVER, "org.postgresql.Driver");
-                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/FoodWeb");
+                settings.put(Environment.URL, "jdbc:postgresql://localhost:5432/food_web");
                 settings.put(Environment.USER, "postgres");
-                settings.put(Environment.PASS, "123456");
+                settings.put(Environment.PASS, "Just4fun4");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.PostgreSQL9Dialect");
 
                 settings.put(Environment.SHOW_SQL, "true");
